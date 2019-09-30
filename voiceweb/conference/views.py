f6e5eec0b7c1ef36e45f1cf7b1ec47657c74fd19
unknown <alswovv@naver.com>
2019-10-01 00:05:57 +0900

    음성 녹음 웹실행 및 json파일 이름 저장

--- voiceweb/conference/views.py
+++ voiceweb/conference/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from . import transcribe_streaming_mic
 # Create your views here.
+import os
+
+def index(request):
+    return render(request, 'vw/index.html')
+
+
+def voice_input_start(requset):
+    #name을 index에서 입력받아서 넘겨줘야함
+    name=0
+    if requset.method == 'POST':
+        name = requset.POST["name_field"]
+        print(name)
+    transcribe_streaming_mic.main(name)
+    return render(requset, 'vw/index.html')
--- voiceweb/voiceweb/settings.py
+++ voiceweb/voiceweb/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'voiceweb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['conference/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
