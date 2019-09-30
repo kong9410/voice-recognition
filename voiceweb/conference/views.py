380fb313f2e4bf73a7fca36bb6fe40a685ad29f8
vkxmfkdl <37204852+vkxmfkdl@users.noreply.github.com>
2019-10-01 00:29:28 +0900

    홈페이지 꾸미기

--- voiceweb/conference/views.py
+++ voiceweb/conference/views.py
@@ -8,16 +8,22 @@ def main(request):
     return render(request, 'vw/main.html')
 
 def conference_analysis(request):
-    vj = ras.voice_json
-    dirname = "./conference/src/text/"
-    filenames = vj.search(dirname)
-    data = vj.fileopen(dirname, filenames[0])
-    ra = ras.read_analysis(data)
-    result = ra.data_summarize(4)
-    keywords = ra.data_keywords(10)
-    print(keywords)
-    context = {
-        "summarize" : result,
-        "keywords" : keywords,
+    context={
+        "summarize":"test",
+        "keywords":"test",
     }
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    print(BASE_DIR)
+    # vj = ras.voice_json
+    # dirname = "./conference/src/text/"
+    # filenames = vj.search(dirname)
+    # data = vj.fileopen(dirname, filenames[0])
+    # ra = ras.read_analysis(data)
+    # result = ra.data_summarize(4)
+    # keywords = ra.data_keywords(10)
+    # print(keywords)
+    # context = {
+    #     "summarize" : result,
+    #     "keywords" : keywords,
+    # }
     return render(request, 'vw/main.html', context=context)
